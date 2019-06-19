6a48b0daf2da381c4b091feb3bbfc7ab21547804
Nelson Castro Dev <00043516@uca.edu.sv>
2019-06-18 23:21:18 -0600

    Added the SpringBoot Intirializr

---
+++ src/main/kotlin/com/uca/bigdreamscoders/bigcinema/BigcinemaApplication.kt
@@ -0,0 +1,11 @@
+package com.uca.bigdreamscoders.bigcinema
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BigcinemaApplication
+
+fun main(args: Array<String>) {
+    runApplication<BigcinemaApplication>(*args)
+}
